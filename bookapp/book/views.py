8d9646814d81be494fd09e0950fc774479fba176
rehshaddy12 <mohamedmikundi12@gmail.com>
2025-01-22 14:18:38 +0300

    finishing to test the modules by mohamed

--- bookapp/book/views.py
+++ bookapp/book/views.py
@@ -3,6 +3,6 @@ from .models import Book
 
 # Create your views here.
 
-def book_listview(request):
+def book_list(request):
     books = Book.objects.all()
     return render(request, 'books/book_list.html', {'books': books})
